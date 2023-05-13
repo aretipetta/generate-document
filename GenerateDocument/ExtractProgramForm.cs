4bfbf279202c8c9c9d0e28f47d7e2614909ae509
areti <aretpett@gmail.com>
2023-05-13 17:32:05 +0300

    adding details in doc file

--- GenerateDocument/ExtractProgramForm.cs
+++ GenerateDocument/ExtractProgramForm.cs
@@ -21,6 +21,7 @@ namespace GenerateDocument
         String nameValidation = "^[a-zA-Z]+(\\s?[a-zA-Z]+){0,3}$";
         String goalValidation = "^[a-zA-Z]+(\\s?[a-zA-Z]+)*$";
         String dateValidation = "^([0-9]{1,2}/){2}[0-9]{4}$";
+        String ageValidation = "^[1-9][0-9]$";
 
         DailyProgram dailyProgram;
         int days;
@@ -43,26 +44,17 @@ namespace GenerateDocument
         // dhmiourgia programmatos kai extract .docx file
         private void button1_Click(object sender, EventArgs e)
         {
-            if (!Regex.IsMatch(textBox1.Text.Trim(), "^[a-zA-Z]+[a-zA-Z0-9]*$"))
-            {
-                //errorProvider1.SetError(textBox1, "Μη έγκυρο όνομα.");
-
-                MessageBox.Show("invalid input.");
-                return;
-            }
-
-            this.Enabled = false;
-            createDoc();
+            validateInputs();
         }
 
         protected void validateInputs()
         {
-            if(!(textBox1.Text.Trim() != null && textBox2.Text.Trim() != null && textBox3.Text.Trim() != null && textBox4.Text.Trim() != null && textBox5.Text.Trim() != null && textBox6.Text.Trim() != null
-                && textBox7.Text.Trim() != null))
-            {
-                MessageBox.Show("Όλα τα πεδία είναι υποχρεωτικά.");
-                return;
-            }
+            //if(textBox1.Text.Trim() == null || textBox2.Text.Trim() != null || textBox3.Text.Trim() == null || textBox4.Text.Trim() == null || textBox5.Text.Trim() == null || textBox6.Text.Trim() == null
+            //    || textBox7.Text.Trim() == null)
+            //{
+            //    MessageBox.Show("Όλα τα πεδία είναι υποχρεωτικά.");
+            //    return;
+            //}
             // else validate data
             if(!Regex.IsMatch(textBox2.Text.Trim(), nameValidation))
             {
@@ -89,12 +81,18 @@ namespace GenerateDocument
                 MessageBox.Show("Μη έγκυρη ημερομηνία λήξης προγράμματος.");
                 return;
             }
-            if (!Regex.IsMatch(textBox5.Text.Trim(), "TODO"))
+            if (!Regex.IsMatch(textBox5.Text.Trim(), ageValidation))
             {
                 MessageBox.Show("Μη έγκυρη ηλικία συνδρομητή/ριας.");
                 return;
             }
-
+            if(!Regex.IsMatch(textBox1.Text.Trim(), docNameValidation))
+            {
+                MessageBox.Show("Μη έγκυρο όνομα αρχείου.");
+                return;
+            }
+            this.Enabled = false;
+            createDoc();
         }
 
         public void createDoc()
@@ -128,6 +126,40 @@ namespace GenerateDocument
                     footerRange.Text = ":)";
                 }
 
+                // details
+                // add table with extra details
+                Microsoft.Office.Interop.Word.Paragraph p = wordDocument.Content.Paragraphs.Add(ref missing);
+                Object styleHeading = Microsoft.Office.Interop.Word.WdBuiltinStyle.wdStyleHeading1;
+                p.Range.set_Style(ref styleHeading);
+                p.Range.ParagraphFormat.Alignment = Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphJustifyMed;
+                p.Range.Text = Environment.NewLine + "ΣΤΟΙΧΕΙΑ";
+                p.Range.InsertParagraphAfter();
+
+                Microsoft.Office.Interop.Word.Table t = wordDocument.Tables.Add(p.Range, 6, 2, ref missing, ref missing); // 6 rows for details (names, age etc) and 2 columns (key-value)
+                t.Borders.Enable = 1;
+                String[] labels = new string[] { "Υπεύθυνος/η γυμναστής/ρια", "Συνδρομητής/ρια", "Στόχος προγράμματος", "Έναρξη προγράμματος", "Λήξη προγράμματος", "Ηλικία συνδρομητή/ριας" };
+                String[] detailsFromForm = new string[] { textBox2.Text.Trim(), textBox3.Text.Trim(), textBox4.Text.Trim(), textBox6.Text.Trim(), textBox7.Text.Trim(), textBox5.Text.Trim() };
+                int idx = 0;
+                foreach (Microsoft.Office.Interop.Word.Row row in t.Rows)
+                {
+                    foreach (Microsoft.Office.Interop.Word.Cell cell in row.Cells)
+                    {
+                        if (cell.ColumnIndex == 1)
+                        {
+                            cell.Range.Text = labels[idx];
+                            cell.Range.Font.Bold = 1;
+                            cell.Shading.BackgroundPatternColor = Microsoft.Office.Interop.Word.WdColor.wdColorYellow;
+                            cell.VerticalAlignment = Microsoft.Office.Interop.Word.WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+                            cell.Range.ParagraphFormat.Alignment = Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                        }
+                        else if (cell.ColumnIndex == 2)
+                        {
+                            cell.Range.Text = detailsFromForm[idx];
+                        }
+                    }
+                    idx++;
+                }
+
                 //adding text to document  
                 wordDocument.Content.SetRange(0, 0);
                 wordDocument.Content.Text = Environment.NewLine;
@@ -135,6 +167,7 @@ namespace GenerateDocument
                 // create tables for each program
                 for (int i = 0; i < days; i++)
                 {
+                    // programmata
                     TableOfProgram tbl = tablesOfProgram[i]; // the whole table with exercises
 
                     Microsoft.Office.Interop.Word.Paragraph par = wordDocument.Content.Paragraphs.Add(ref missing);
@@ -149,7 +182,7 @@ namespace GenerateDocument
                     Microsoft.Office.Interop.Word.Table table = wordDocument.Tables.Add(par.Range, tbl.Exercises.Count + 1 + 3, Enum.GetValues(typeof(ColumnEnum)).Length, ref missing, ref missing);
                     table.Borders.Enable = 1;
                     //table.AutoFitBehavior(Microsoft.Office.Interop.Word.WdAutoFitBehavior.wdAutoFitWindow); //.wdAutoFitWindow
-                  //  table.PreferredWidthType = Microsoft.Office.Interop.Word.WdPreferredWidthType.wdPreferredWidthPercent; // wordDocument.Sections[0].PageSetup.PageWidth - 20;
+                    //  table.PreferredWidthType = Microsoft.Office.Interop.Word.WdPreferredWidthType.wdPreferredWidthPercent; // wordDocument.Sections[0].PageSetup.PageWidth - 20;
                     //firstTable.PreferredWidth = 30;
                     String[] exercisesToRowVector = tbl.listToRowVector();
                     int counter = 0;
@@ -165,7 +198,7 @@ namespace GenerateDocument
                                 //else cell.SetWidth(cell.Column.Width, Microsoft.Office.Interop.Word.WdRulerStyle.wdAdjustProportional);
                                 cell.Range.Text = ((ColumnEnum)cell.ColumnIndex).ToString();
                                 cell.Range.Font.Bold = 1;
-                              //  cell.Range.Font.Size = 8;
+                                //  cell.Range.Font.Size = 8;
                                 //other format properties goes here  
                                 //cell.Range.Font.Name = "verdana";
                                 //   cell.Column.AutoFit();
@@ -177,7 +210,7 @@ namespace GenerateDocument
                                 cell.Range.ParagraphFormat.Alignment = Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
                             }
                         }
-                        else if(row.Index == 2) // prothermansi
+                        else if (row.Index == 2) // prothermansi
                         {
                             List<String> items = new List<string>(ConfigurationManager.AppSettings["WARM-UP"].Split(';'));
                             int j = 0;
@@ -190,7 +223,7 @@ namespace GenerateDocument
                                 j++;
                             }
                         }
-                        else if(row.Index == tbl.Exercises.Count + 3)
+                        else if (row.Index == tbl.Exercises.Count + 3)
                         {
                             List<String> items = new List<string>(ConfigurationManager.AppSettings["AEROBIC"].Split(';'));
                             int j = 0;
@@ -203,7 +236,7 @@ namespace GenerateDocument
                                 j++;
                             }
                         }
-                        else if(row.Index == tbl.Exercises.Count + 4)
+                        else if (row.Index == tbl.Exercises.Count + 4)
                         {
                             List<String> items = new List<string>(ConfigurationManager.AppSettings["STRETCHING"].Split(';'));
                             int j = 0;
@@ -224,7 +257,7 @@ namespace GenerateDocument
                                 //else cell.SetWidth(cell.Column.Width, Microsoft.Office.Interop.Word.WdRulerStyle.wdAdjustProportional);
                                 cell.Range.Font.Size = 9;
                                 //cell.Column.AutoFit();
-                                
+
                                 cell.Range.Text = exercisesToRowVector[counter];
                                 counter++;
                             }
